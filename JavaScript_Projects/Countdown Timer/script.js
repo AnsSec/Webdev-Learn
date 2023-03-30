6db34435d55e58d26a387bc6b767a34436624f68
Anshul Vishwakarma <72655193+AnsSec@users.noreply.github.com>
2023-03-30 14:42:03 +0530

    update script.js

--- JavaScript_Projects/Countdown Timer/script.js
+++ JavaScript_Projects/Countdown Timer/script.js
@@ -1,6 +1,8 @@
-const endDate="30 April 2023 01:24 PM"
+const endDate="31 March 2023 01:24 PM"
 const newDate=document.querySelector("#end-date")
 newDate.innerText=endDate;
+
+const inputs=document.querySelectorAll("input")
 
 function clock(){
     const end=new Date(endDate);
@@ -8,8 +10,24 @@ function clock(){
     const diff=(end-now)/1000;
     console.log(diff)
 
-    // conver to days 
+    // for Non negative numbers 
+    if(diff<0) return;
 
+    // convert to days 
+    inputs[0].value=Math.floor(diff/3600/24);
+    // convert to hours
+    inputs[1].value=Math.floor(diff/3600)%24;
+    // convert to minutes
+    inputs[2].value=Math.floor(diff/60)%60;
+    // convert to second 
+    inputs[3].value=Math.floor(diff)%60;
 }
 
+// initial call 
 clock();
+
+// setInterval for update value without page refresh
+setInterval(()=>{
+    clock();
+},1000
+)
