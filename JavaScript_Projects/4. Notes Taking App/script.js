1a40242f1ea1ed09bf20881923b2efa8757fb78c
Anshul Vishwakarma <72655193+AnsSec@users.noreply.github.com>
2023-04-01 16:37:31 +0530

    update js

---
+++ JavaScript_Projects/4. Notes Taking App/script.js
@@ -0,0 +1,64 @@
+const addBtn = document.querySelector('#addBtn')
+const main=document.querySelector('#main')
+
+
+const saveNotes=()=>{
+    const note=document.querySelectorAll('.note textarea')
+    const data=[];
+    note.forEach((note)=>{
+        data.push(note.value)
+    })
+    if(data.length===0){
+        localStorage.removeItem('notes')
+    }else{
+        localStorage.setItem('notes',JSON.stringify(data))
+    }
+    localStorage.setItem('notes',JSON.stringify(data))
+    
+}
+
+addBtn.addEventListener('click', function () {
+    addNote();
+});
+
+
+
+const addNote = (text="") => {
+    const note = document.createElement("div")
+    note.classList.add("note")
+    note.innerHTML = `
+            <div class="tool">
+                <i class="save fas fa-save"></i>
+                <i class="trash fas fa-trash"></i>
+            </div>
+            <textarea>${text}</textarea>
+    `;
+    note.querySelector('.trash').addEventListener('click',function(){
+        note.remove();
+        saveNotes();
+    })
+    note.querySelector('.save').addEventListener('click',function(){
+        saveNotes();
+    })
+    note.querySelector('textarea').addEventListener('focusout',function(){
+        saveNotes();
+    })
+    main.appendChild(note);
+    saveNotes();
+    
+}
+
+(
+    function(){
+        const lsnotes=JSON.parse(localStorage.getItem('notes'));
+        if(lsnotes===null){
+            addNote();
+        }else{
+            lsnotes.forEach((lsnotes)=>{
+                addNote(lsnotes);
+            })
+        }
+        
+        
+    }
+)()
