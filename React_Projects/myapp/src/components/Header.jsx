73a5958390ee60a9329bb7cf893c47201bcd8e5d
Anshul Vishwakarma <72655193+AnsSec@users.noreply.github.com>
2023-04-03 19:34:52 +0530

    update

--- React_Projects/myapp/src/components/Header.jsx
+++ React_Projects/myapp/src/components/Header.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import "../style/Header.css";
-
+import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
     <nav>
-      <a href="/">Home</a>
-      <a href="/about">About</a>
-      <a href="/contact">Contact</a>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
     </nav>
   )
 }
---
+++ React_Projects/myapp/src/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <h1>This Is Home</h1>
+  )
+}
+
+export default Home
