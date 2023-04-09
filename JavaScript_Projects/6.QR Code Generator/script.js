a45671ba15994598505c0017d93ad59717d7954b
Anshul Vishwakarma <72655193+AnsSec@users.noreply.github.com>
2023-04-09 15:21:32 +0530

    update

--- JavaScript_Projects/6.QR Code Generator/script.js
+++ JavaScript_Projects/6.QR Code Generator/script.js
@@ -1,38 +1,42 @@
-const qrText=document.querySelector('#qr-Text')
-const sizes=document.querySelector('#sizes')
-const qrContainer=document.querySelector('#qr-Container')
-const error=document.querySelector('.error');
+const qrText = document.querySelector('#qr-Text')
+const sizes = document.querySelector('#sizes')
+const qrContainer = document.querySelector('#qr-Container')
+const error = document.querySelector('.error');
 
 //generate button
-const  generate=()=>{
-    if(qrText.value.length>0){
-        generateQRcode();
-        error.classList.remove('active')
-    }else
-    {
-        error.classList.add('active')
-    }
-   
+const generate = () => {
+    isEmptyInput();
 };
 
 // download button
-const download=()=>{
-    
+const download = () => {
+
 }
 
-sizes.addEventListener('change',(e)=>{
-    size=e.target.value;
+const isEmptyInput = () => {
+    // if (qrText.value.length > 0) {
+    //     generateQRcode();
+    //     error.classList.remove('active')
+    // } else {
+        
+    // }
+    qrText.value.length>0?[generateQRcode(), error.classList.remove('active')]:error.classList.add('active');
+}
+
+sizes.addEventListener('change', (e) => {
+    size = e.target.value;
+    isEmptyInput();
 })
 
-let size=sizes.value;
-const generateQRcode=()=>{
-    qrContainer.innerHTML="";
-    new QRCode(qrContainer,{
-        text:qrText.value,
-        height:size,
-        width:size,
-        colorLight:'#FFFFFF',
-        colorDark:'#141D2F',
+let size = sizes.value;
+const generateQRcode = () => {
+    qrContainer.innerHTML = "";
+    new QRCode(qrContainer, {
+        text: qrText.value,
+        height: size,
+        width: size,
+        colorLight: '#FFFFFF',
+        colorDark: '#141D2F',
 
     })
 }
