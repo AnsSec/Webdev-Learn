60bfb64d7b582e5aea2904442a7d00a3551baf30
Anshul Vishwakarma <72655193+AnsSec@users.noreply.github.com>
2023-04-09 14:01:27 +0530

    js update

--- JavaScript_Projects/6.QR Code Generator/script.js
+++ JavaScript_Projects/6.QR Code Generator/script.js
@@ -1,1 +1,6 @@
-get
+const qrText=document.querySelector('#qr-Text')
+const sizes=document.querySelector('#sizes')
+const generate=document.querySelector('#generate')
+const download=document.querySelector('#download')
+
+
