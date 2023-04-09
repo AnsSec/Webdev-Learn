70a026c9948c1cfa6a34adcc3db7707d472f0f8d
Anshul Vishwakarma <72655193+AnsSec@users.noreply.github.com>
2023-04-09 15:07:33 +0530

    ewfrwfrw

--- JavaScript_Projects/6.QR Code Generator/script.js
+++ JavaScript_Projects/6.QR Code Generator/script.js
@@ -1,8 +1,38 @@
 const qrText=document.querySelector('#qr-Text')
 const sizes=document.querySelector('#sizes')
-const generate=document.querySelector('#generate')
-const download=document.querySelector('#download')
 const qrContainer=document.querySelector('#qr-Container')
+const error=document.querySelector('.error');
 
+//generate button
+const  generate=()=>{
+    if(qrText.value.length>0){
+        generateQRcode();
+        error.classList.remove('active')
+    }else
+    {
+        error.classList.add('active')
+    }
+   
+};
 
+// download button
+const download=()=>{
+    
+}
 
+sizes.addEventListener('change',(e)=>{
+    size=e.target.value;
+})
+
+let size=sizes.value;
+const generateQRcode=()=>{
+    qrContainer.innerHTML="";
+    new QRCode(qrContainer,{
+        text:qrText.value,
+        height:size,
+        width:size,
+        colorLight:'#FFFFFF',
+        colorDark:'#141D2F',
+
+    })
+}
