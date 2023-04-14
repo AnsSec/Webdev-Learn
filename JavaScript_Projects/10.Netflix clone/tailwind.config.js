d4affbe9c506c8cafa4a6c45973487f164d33e2c
Anshul Vishwakarma <72655193+AnsSec@users.noreply.github.com>
2023-04-14 18:44:33 +0530

    ..

---
+++ JavaScript_Projects/10.Netflix clone/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontFamily:{
+      roboto:['Roboto','sans-serif']
+    },
+    extend: {
+      backgroundImage: {
+        'banner': "url('assets/images/banner.jpg')"
+      }
+    },
+  },
+  plugins: [],
+}
