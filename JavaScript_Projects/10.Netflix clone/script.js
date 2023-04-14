e1adc4802804cfd1baaf2dabf5e002fb37272ee3
Anshul Vishwakarma <72655193+AnsSec@users.noreply.github.com>
2023-04-14 19:51:54 +0530

    ..

---
+++ JavaScript_Projects/10.Netflix clone/script.js
@@ -0,0 +1,13 @@
+const navBar=document.querySelector('#navBar');
+
+
+const handleScroll = () => {
+    const scrollvall=window.scrollY;
+    navBar.classList.remove('active');
+    if (scrollvall>1){
+        navBar.classList.add('active');
+    }else{
+        navBar.classList.remove('active');
+    }
+};
+window.addEventListener('scroll',handleScroll);
