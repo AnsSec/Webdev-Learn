06df2eb08084a8d824213568699eea6d466b829c
Anshul Vishwakarma <72655193+AnsSec@users.noreply.github.com>
2023-04-16 17:11:30 +0530

    ..

--- JavaScript_Projects/11.Netflix clone/script.js
+++ JavaScript_Projects/11.Netflix clone/script.js
@@ -1,5 +1,40 @@
-const ApiKey=require('dotenv');
-ApiKey.config({path:'config.env'})
+const apiKey='47530343367936e133c62869ed49faf8';//use api from env file
+
+const ApiEndpoint='https://api.themoviedb.org/3';
+const Apipath={
+    fetchAllCategories:`${ApiEndpoint}/genre/movie/list?api_key=${apiKey}&language=en-US`
+}
+
+
+//Bootup
+const init=()=>{
+    fetch_And_Build_AllSection();
+}
+
+const fetch_And_Build_AllSection=()=>{
+    fetch(Apipath.fetchAllCategories)
+    .then(res=>res.json())
+    .then(res=>{
+        const categories=res.genres;
+        if(Array.isArray(categories)&&categories.length)
+        {
+           categories.forEach(categories=>{
+            fetchAndBuildMovieSection(categories);
+           })
+        }
+        // console.table(categories) 
+    })
+    .catch(err=>console.log(err))
+}
+
+const fetchAndBuildMovieSection=(categories)=>{
+    confirm
+}
+
+window.addEventListener('load',()=>{
+    init();
+})
+
 // scroll animation 
 const navBar=document.querySelector('#navBar');
 const handleScroll = () => {
