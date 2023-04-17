0b751e66ef56e00fa21fc0dc82eec3a24d5b2e2b
Anshul Vishwakarma <72655193+AnsSec@users.noreply.github.com>
2023-04-17 14:59:43 +0530

    ..

--- JavaScript_Projects/11.Netflix clone/script.js
+++ JavaScript_Projects/11.Netflix clone/script.js
@@ -1,35 +1,51 @@
 const apiKey='47530343367936e133c62869ed49faf8';
 
-const ApiEndpoint='https://api.themoviedb.org/3';
-const Apipath={
-    fetchAllCategories:`${ApiEndpoint}/genre/movie/list?api_key=${apiKey}&language=en-US`
-}
+const baseURL='https://api.themoviedb.org/3';
+const imageBaseURL='https://image.tmdb.org/t/p/original';
 
+
+//categories 
+const contentCategories={
+    genre:'/genre/movie/list?api_key=',
+    treandingAllDay:'/trending/all/day?api_key=',
+    leatest:'/movie/latest?api_key=',
+    nowPlaying:'/movie/now_playing?api_key=',
+    popular:'/movie/popular?api_key=',
+    topRated:'/movie/top_rated?api_key=',
+    upComing:'/movie/upcoming?api_key=',
+
+}
 
 //Bootup
 const init=()=>{
-    fetch_And_Build_AllSection();
+    fetch_And_Build_Genre();
 }
 
-const fetch_And_Build_AllSection=()=>{
-    fetch(Apipath.fetchAllCategories)
-    .then(res=>res.json())
-    .then(res=>{
-        const categories=res.genres;
-        if(Array.isArray(categories)&&categories.length)
-        {
-           categories.forEach(categories=>{
-            fetchAndBuildMovieSection(categories);
-           })
-        }
-        // console.table(categories) 
+const content=document.querySelector('#content')
+
+const fetch_And_Build_Genre=()=>{
+    fetch(`${baseURL}${contentCategories.genre}${apiKey}`)
+    .then(response => response.json())
+    .then(result=>{
+        const categories=result.genres
+        categories.forEach((val)=>{
+            let contentCategories=document.createElement('div');
+            contentCategories.classList.add('carousel')
+            contentCategories.innerHTML=val.name;
+            content.appendChild(contentCategories)
+            console.log(`${val.id}=${val.name}`);
+        })
     })
-    .catch(err=>console.log(err))
+    .catch(error=>console.log('error',error));
+
 }
 
-const fetchAndBuildMovieSection=(categories)=>{
-    console.log(categories)
-}
+
+
+// const fetchAndBuildMovieSection=(categories)=>{
+//     // let categories_name=categories.id
+//     console.log(categories)
+// }
 
 window.addEventListener('load',()=>{
     init();
