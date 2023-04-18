d3d3cadd203956c5bce7b0847f7e4e9422b4e3fc
Anshul Vishwakarma <72655193+AnsSec@users.noreply.github.com>
2023-04-18 14:40:59 +0530

    ..

--- React_Projects/plan-trip/src/App.js
+++ React_Projects/plan-trip/src/App.js
@@ -1,11 +1,14 @@
-import React, {useState} from "react";
+import React from "react";
+import Tours from "./components/Tours";
 
+import './index.css'
 
 const App = () => {
-
   return (
-    <>
-    </>
+    <div>
+      <h1>Plan With Love</h1>
+      <Tours></Tours>
+    </div>
   )
 };
 
