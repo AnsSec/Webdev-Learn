791506b9e3dca0ae02a34891ea8f16f2295937c8
Anshul Vishwakarma <72655193+AnsSec@users.noreply.github.com>
2023-04-18 13:50:18 +0530

    .

--- React_Projects/counterapp/src/App.js
+++ React_Projects/counterapp/src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <>
-    
-    </>
+    <div className="w-[100vw] h-[100vh] flex justify-center items-center flex-col bg-[#344151]  gap-10">
+      <h1 className="text-[#0398d4] font-medium text-2xl">Increment && Decrement</h1>
+
+      <div>
+        <button>-</button>
+        <div></div>
+        <button>+</button>
+      </div>
+
+      <button>Reset</button>
+    </div>
   );
 }
 
