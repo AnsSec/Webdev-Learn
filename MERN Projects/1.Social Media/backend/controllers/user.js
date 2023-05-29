8107e54df0aeef446214cba6b0e011a9b090c9d1
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-05-29 20:44:29 +0530

    .

--- MERN Projects/1.Social Media/backend/controllers/user.js
+++ MERN Projects/1.Social Media/backend/controllers/user.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 
-exports.registerUser = async (req,res) => {
+exports.registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
 
@@ -10,20 +10,65 @@ exports.registerUser = async (req,res) => {
         success: false,
         message: "User already exists",
       });
-    };
+    }
 
-    user=await User.create({ name:name , email:email , password:password});
+    user = await User.create({ name: name, email: email, password: password });
 
     res.status(200).json({
-        success: true,
-        message:"user successfully created",
-      });
-  } 
-  catch (error) {
+      success: true,
+      message: "user successfully created",
+    });
+  } catch (error) {
     console.error(error);
     return res.status(404).json({
-        success:false,
-        message:'User Cannot Be Registered please try again later'
-    })
+      success: false,
+      message: "User Cannot Be Registered please try again later",
+    });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      return res.status(400).json({
+        status: false,
+        message: "user does not exist",
+      });
+    }
+
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        status: false,
+        message: "incorrect password",
+      });
+    }
+
+    const token = await user.generateToken();
+
+    const options={
+        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+        httpOnly:true
+    };
+
+    res
+      .status(200)
+      .cookie("token", token, options)
+      .json({
+        status: true,
+        user,
+        token,
+      });
+  } catch (error) {
+    // console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
   }
 };
--- MERN Projects/1.Social Media/backend/models/User.js
+++ MERN Projects/1.Social Media/backend/models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt=require("bcrypt");
+const jwt=require("jsonwebtoken");
+require("dotenv").config({path:"config/.env"})
 
 const userSchema = new mongoose.Schema({
   name:{
@@ -52,5 +54,14 @@ userSchema.pre("save",async function(next){
     this.password=await bcrypt.hash(this.password,10);
   }
   next();
-})
+});
+
+userSchema.methods.matchPassword=async function (password){
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateToken=function(){
+  return jwt.sign({_id:this._id},process.env.JWT_SECRET)
+};
+
 module.exports = mongoose.model("User", userSchema);
