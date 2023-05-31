e0dfdd5cb27233a8ecc6befa698c932479d51055
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-05-31 19:35:30 +0530

    ..

--- MERN Projects/1.Social Media/backend/routes/user.js
+++ MERN Projects/1.Social Media/backend/routes/user.js
@@ -8,6 +8,8 @@ const {
   logout,
   updatePassword,
   updateProfile,
+  deleteMyProfile,
+  myProfile,
 } = require("../controllers/user");
 
 const { isAuthenticated } = require("../middlewares/auth");
@@ -18,5 +20,8 @@ router.get("/logout", logout);
 router.route("/follow/:id").get(isAuthenticated, followUser);
 router.route("/update/password").put(isAuthenticated, updatePassword);
 router.route("/update/profile").put(isAuthenticated, updateProfile);
+router.route("/delete/me").delete(isAuthenticated, deleteMyProfile);
+router.route("/me").delete(isAuthenticated, myProfile);
+
 
 module.exports = router;
